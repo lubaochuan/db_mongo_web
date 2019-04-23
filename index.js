9dd8db9b1fb0c179a00dc1047a69e476a677966c
Baochuan Lu <blu@sbuniv.edu>
2019-04-23 15:30:36 +0000

    generate response from query parameters

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 
 app.use('/', (req, res) => {
-  res.send('Hello World!');
+  var name = req.query.name; // e.g. /?name=devesh
+  res.status(200).type('html');
+  if (name) {
+    res.write('Hi, ' + name + " it's nice to see you.");
+  } else {
+    res.write('Welcome, guest!');
+  }
+  res.end();
 });
 
 var port = process.env.PORT;
