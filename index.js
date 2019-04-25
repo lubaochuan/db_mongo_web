7bb7b1e3cc60398fe12a45b8132f25ba225df785
Baochuan Lu <blu@sbuniv.edu>
2019-04-25 15:52:42 +0000

    add starter code

--- index.js
+++ index.js
@@ -1,28 +1,88 @@
 var express = require('express');
 var app = express();
 
-app.use('/about', (req, res) => {
-  res.send('This is the about page.');
+app.set('view engine', 'ejs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var Book = require('./Book.js');
+
+app.use('/createbook', (req, res) => {
+  var newBook = new Book(req.body);
+  newBook.save( (err) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else {
+      res.send('Created new book');
+    }
+  });
 });
 
-app.use('/login', (req, res) => {
-  res.send('This is the login page.');
-});
-
-app.use( /*default*/ (req, res) => {
-  res.status(404).send('Not found!');
-});
-
-app.use('/', (req, res) => {
-  var name = req.query.name; // e.g. /?name=devesh
-  res.status(200).type('html');
-  if (name) {
-    res.write('Hi, ' + name + " it's nice to see you.");
+app.use('/search', (req, res) => {
+  if (req.body.which == 'all') {
+    searchAll(req, res);
+  } else if (req.body.which == 'any') {
+    searchAny(req, res);
   } else {
-    res.write('Welcome, guest!');
+    searchAll(req, res);
   }
-  res.end();
 });
+
+function searchAny(req, res) {
+  var terms = [];
+
+  if (req.body.title) {
+  terms.push( { title: { $regex : req.body.title } });
+  }
+  if (req.body.name) {
+    terms.push( { 'authors.name' : req.body.name });
+  }
+  if (req.body.year) {
+    terms.push( { year: req.body.year });
+  }
+  var query = { $or : terms };
+
+  Book.find( query, (err, books) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else {
+      res.render('books', { books: books });
+    }
+  }).sort( { 'title': 'asc' } );
+}
+
+function searchAll(req, res) {
+  var query = {};
+  if (req.body.title) {
+      query.title = req.body.title;
+  }
+  if (req.body.name) {
+    query['authors.name'] = req.body.name;
+  }
+  if (req.body.year) {
+    query.year = req.body.year;
+  }
+
+  Book.find( query, (err, books) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else {
+      res.render('books', { books: books });
+    }
+  });
+}
+
+app.use('/public', express.static('public'));
+
+/*
+app.listen(3000,  () => {
+  console.log('Listening on port 3000');
+});
+*/
 
 var port = process.env.PORT;
 var IP = process.env.IP;
